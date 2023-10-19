b4e353755ce9296b83cd4000a58e481f6e451709
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 14:37:28 -0300

    CSS-in-JS - Criando interface NewGroup

--- App.tsx
+++ App.tsx
@@ -2,8 +2,9 @@ import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 
+import { NewGroup } from '@screens/NewGroup';
+
 import { Loading } from '@components/Loading';
-import { Groups } from '@screens/Groups';
 
 import theme from './src/theme';
 
@@ -12,17 +13,15 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
 
-      {
-        fontsLoaded
-          ? <Groups />
-          : <Loading />
-      }
+        {fontsLoaded ? <NewGroup /> : <Loading />}
+      </>
     </ThemeProvider>
   );
 }
