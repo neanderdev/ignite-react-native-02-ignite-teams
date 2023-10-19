3f8ec9bf9d58e4ead2caceb78f1ccd51dcf7ca44
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 13:58:10 -0300

    CSS-in-JS - Fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,3 +1,5 @@
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components/native';
 
 import { Groups } from '@screens/Groups';
@@ -5,9 +7,15 @@ import { Groups } from '@screens/Groups';
 import theme from './src/theme';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {
+        fontsLoaded
+          ? <Groups />
+          : <ActivityIndicator />
+      }
     </ThemeProvider>
   );
 }
