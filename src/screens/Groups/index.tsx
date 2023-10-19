d59489906afcd5f9d8cf5a3ed6c97de9a22e111a
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 16:16:01 -0300

    Local Storage - Refactor do Loading

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -30,12 +30,12 @@ export function Groups() {
             const data = await groupsGetAll();
 
             setGroups(data);
-
-            setIsLoading(false);
         } catch (error) {
             console.log(error);
 
             Alert.alert('Turmas', 'Não foi possível carregar as turmas');
+        } finally {
+            setIsLoading(false);
         }
     }
 
