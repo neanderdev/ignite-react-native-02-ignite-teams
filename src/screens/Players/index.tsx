581b0f52869001684892c4401db803bad2e03bbd
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 15:01:15 -0300

    CSS-in-JS - Finalizando lista de Players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { Button } from '@components/Button';
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 
 export function Players() {
     const [team, setTeam] = useState('Time A')
-    const [players, setPlayers] = useState(['Rodrigo', 'Vini'])
+    const [players, setPlayers] = useState([])
 
     return (
         <Container>
@@ -62,6 +64,16 @@ export function Players() {
                         onRemove={() => { }}
                     />
                 )}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Não há pessoas nesse time" />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[{ paddingBottom: 100 }, players.length === 0 && { flex: 1 }]}
+            />
+
+            <Button
+                title="Remover Turma"
+                type="SECONDARY"
             />
         </Container>
     )
