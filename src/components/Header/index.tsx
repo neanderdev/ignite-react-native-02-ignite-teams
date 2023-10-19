d29522c9c6f46ad397fe18e21dd53796ce91753a
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 15:11:20 -0300

    Navegação - Implementando o voltar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from "@react-navigation/native";
+
 import logoImg from '@assets/logo.png';
 
 import { BackButton, BackIcon, Container, Logo } from "./styles";
@@ -7,11 +9,17 @@ type Props = {
 }
 
 export function Header({ showBackButton = false }: Props) {
+    const navigation = useNavigation()
+
+    function handleGoBack() {
+        navigation.navigate('groups')
+    }
+
     return (
         <Container>
             {
                 showBackButton &&
-                <BackButton>
+                <BackButton onPress={handleGoBack}>
                     <BackIcon />
                 </BackButton>
             }
