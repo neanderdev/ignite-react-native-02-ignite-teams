0c3e2bf0759bcbf5fc305058f214b529a8869c6b
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 15:35:18 -0300

    Local Storage - Abrindo turmas

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -30,6 +30,10 @@ export function Groups() {
         }
     }
 
+    function handleOpenGroup(group: string) {
+        navigation.navigate('players', { group })
+    }
+
     useFocusEffect(useCallback(() => {
         fetchGroups()
     }, []))
@@ -49,6 +53,7 @@ export function Groups() {
                 renderItem={({ item }) => (
                     <GroupCard
                         title={item}
+                        onPress={() => handleOpenGroup(item)}
                     />
                 )}
                 contentContainerStyle={groups.length === 0 && { flex: 1 }}
