2ae6792e0b9a81c194c40c2e2937f279ed27e0f1
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 15:46:57 -0300

    Local Storage - Carregando Players pelo Grupo e Time

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -11,8 +11,9 @@ import { Input } from "@components/Input";
 import { ListEmpty } from '@components/ListEmpty';
 import { PlayerCard } from '@components/PlayerCard';
 
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
 import { playerAddByGroup } from '@storage/player/playerAddByGroup';
-import { playersGetByGroup } from '@storage/player/playersGetByGroup';
+import { playersGetByGroupAndTeam } from '@storage/player/playersGetByGroupAndTeam';
 
 import { AppError } from '@utils/AppError';
 
@@ -25,7 +26,7 @@ type RouteParams = {
 export function Players() {
     const [newPlayerName, setNewPlayerName] = useState('');
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState([]);
+    const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
 
     const route = useRoute();
 
@@ -43,20 +44,29 @@ export function Players() {
 
         try {
             await playerAddByGroup(newPlayer, group);
-
-            const players = await playersGetByGroup(group);
-
-            console.log(players);
         } catch (error) {
             if (error instanceof AppError) {
                 Alert.alert('Nova pessoa', error.message);
             } else {
                 console.log(error);
 
-                Alert.alert('Nova pessoa', 'Não foi possível adicionar.')
+                Alert.alert('Nova pessoa', 'Não foi possível adicionar.');
             }
         }
     }
+
+    async function fetchPlayersByTeam() {
+        try {
+            const playersByTeam = await playersGetByGroupAndTeam(group, team);
+
+            setPlayers(playersByTeam)
+        } catch (error) {
+            console.log(error);
+
+            Alert.alert('Pessoas', 'Não foi possível carregar as pessoas do time selecionado.');
+        }
+    }
+
 
     return (
         <Container>
