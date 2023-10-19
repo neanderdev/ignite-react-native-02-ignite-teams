1cf115223c618e6d4f6aba49cb6c9a0eee555f7e
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 14:35:23 -0300

    CSS-in-JS - Criando Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
+import { Button } from '@components/Button';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
@@ -32,6 +33,10 @@ export function Groups() {
                 ListEmptyComponent={() => (
                     <ListEmpty message="Que tal cadastrar a primeira turma?" />
                 )}
+            />
+
+            <Button
+                title='Criar nova turma'
             />
         </Container>
     );
