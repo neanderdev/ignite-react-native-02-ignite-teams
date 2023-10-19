6e791849180f87b16706caf6649b0e4a264d35ea
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 15:08:58 -0300

    Navegação - Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
@@ -11,6 +12,12 @@ import { Container } from './styles';
 
 export function Groups() {
     const [groups, setGroups] = useState<string[]>([]);
+
+    const navigation = useNavigation()
+
+    function handleNewGroup() {
+        navigation.navigate('new')
+    }
 
     return (
         <Container>
