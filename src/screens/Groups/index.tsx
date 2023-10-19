c09c4c44df9832004a001bf534b85b10467f74e0
Neander de Souza <neander.faria@hotmail.com>
2023-10-19 14:34:13 -0300

    CSS-in-JS - Componente List Empty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,11 +4,12 @@ import { FlatList } from 'react-native';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
 export function Groups() {
-    const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+    const [groups, setGroups] = useState<string[]>([]);
 
     return (
         <Container>
@@ -26,6 +27,10 @@ export function Groups() {
                     <GroupCard
                         title={item}
                     />
+                )}
+                contentContainerStyle={groups.length === 0 && { flex: 1 }}
+                ListEmptyComponent={() => (
+                    <ListEmpty message="Que tal cadastrar a primeira turma?" />
                 )}
             />
         </Container>
